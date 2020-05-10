beaea80b5ba4bf00df90daa2bdd5397fb6385177
Diego Fernandes <diegofernandesp@gmail.com>
2020-05-10 09:56:08 -0300

    New file for keydownlistener module

--- js/game.js
+++ js/game.js
@@ -111,40 +111,6 @@ const game = createGame();
 const keyDownListener = createKeyDownListener();
 keyDownListener.subscribe(game.movePlayer);
 
-function createKeyDownListener(){
-    const state = {
-        observers: []
-    }
-
-    function subscribe(observerFunction){
-        state.observers.push(observerFunction);
-    }
-
-    function notifyAll(command){
-        console.log("notifying "+state.observers.length+" observers");
-        for (const observerFunction of state.observers){
-            observerFunction(command)
-        }
-    }
-
-    window.addEventListener("keydown", keyDownHandler);
-
-    function keyDownHandler(e) {
-        var keyPressed = e.key;
-    
-        const command = {
-            playerId: "player1",
-            keyPressed
-        }
-    
-        notifyAll(command);
-    }
-
-    return{
-        subscribe
-    }
-}
-
 renderScreen();
 
 function renderScreen() {
---
+++ js/keydownlistener.js
@@ -0,0 +1,33 @@
+function createKeyDownListener(){
+    const state = {
+        observers: []
+    }
+
+    function subscribe(observerFunction){
+        state.observers.push(observerFunction);
+    }
+
+    function notifyAll(command){
+        console.log("notifying "+state.observers.length+" observers");
+        for (const observerFunction of state.observers){
+            observerFunction(command)
+        }
+    }
+
+    window.addEventListener("keydown", keyDownHandler);
+
+    function keyDownHandler(e) {
+        var keyPressed = e.key;
+    
+        const command = {
+            playerId: "player1",
+            keyPressed
+        }
+    
+        notifyAll(command);
+    }
+
+    return{
+        subscribe
+    }
+}
