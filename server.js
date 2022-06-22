263fe2894cb187201e9935b8c2a276893b7ecf82
Diego <diego@linter.com.br>
2022-06-22 17:53:44 -0300

    Novo teste com porta

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ sockets.on('connection', (socket) => {
 
 app.use(express.static('public'))
 
-const port = 443;
+const port = process.env.port;
 server.listen(port, () => {
     console.log("Server Listening on port " + port)
 })
